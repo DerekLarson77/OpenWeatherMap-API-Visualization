e38222ec9c72f55bc39b977e1121d9e709c6dcdd
DerekLarson77 <DerekLarson77@gmail.com>
2021-04-27 12:35:38 -0700

    api_keys
    api_keys

--- VacationPy/api_keys.py
+++ VacationPy/api_keys.py
@@ -2,4 +2,4 @@
 weather_api_key = "0abd534c4439714ac6302b3ebb940486"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyD9_OYNMKv_6h6AE63NX299Ccm6ko_OhxM"
--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,4 @@
 # OpenWeatherMap API Key
 weather_api_key = "0abd534c4439714ac6302b3ebb940486"
 
-# Google API Key
-g_key = "YOUR KEY HERE!"
+
